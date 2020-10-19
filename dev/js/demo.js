0a0808ba24d5a2a9f31713c4c7f363dda1a9b3db
kaylesgg <63324097+kaylesgg@users.noreply.github.com>
2020-10-19 09:18:57 -0400

    ahhh

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,14 @@
 import {gsap} from "gsap";
 
+gsap.set("#red-box",{transformOrigin: "center"});
+
 // only here to make codeKit be quiet!
-console.log(gsap);
+const redBoxTL = gsap.timeline();
+redBoxTL.from("#red-box",{duration:1,rotation:180, x:-600, ease: "none"})
+        .to("#red-box",{duration:1,rotation: -180, x:600, ease: "none"});
+
+export function redBoxAnimation(){
+    return redBoxTL;
+}
+
+
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,5 @@
-import * as Demo from './demo.js';
+import {gsap} from "gsap";
+import {redBoxAnimation} from "./demo.js"
 
-console.log(Demo);
+const mainTL = gsap.timeline();
+mainTL.add(redBoxAnimation());
